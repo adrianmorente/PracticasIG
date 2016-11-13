0065f203c319954719d74e1b56d3e12fce448f1c
Adrian Morente <adrian95morente@gmail.com>
2016-11-14 00:29:40 +0100

    Empezando práctica 3

---
+++ P3/esfera.h
@@ -0,0 +1,14 @@
+#ifndef _ESFERA_H
+#define _ESFERA_H
+
+#include "objeto3D.h"
+using namespace std;
+
+class Esfera : public Objeto3D {
+
+  public:
+    Esfera();
+
+};
+
+#endif
---
+++ P3/robot.h
@@ -0,0 +1,15 @@
+#ifndef _OBJETO_REVOLUCION_H
+#define _OBJETO_REVOLUCION_H
+
+#include "objeto3D.h"
+using namespace std;
+
+class Robot : public Objeto3D {
+
+  private:
+
+  public:
+    Robot();
+};
+
+#endif
